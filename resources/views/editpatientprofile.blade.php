5e49e93f5ff14fd188599fdf5eca274fb64518c4
EngHamda <eng.nhamda@yahoo.com>
2016-06-11 17:48:50 +0200

    solve errors

--- resources/views/editpatientprofile.blade.php
+++ resources/views/editpatientprofile.blade.php
@@ -15,11 +15,6 @@
         {!!  Form::label('birth_date', 'birth_date')!!}
             {!! Form::text('birth_date',$value=$user->birth_date,['id' => 'datepicker-1','class'=>'form-control'])!!}
 
-        {!!  Form::label('gender', 'gender')!!}
-            {!! Form::text('gender',$value=$user->gender,['class' => 'form-control'])!!}
-                {!!  Form::radio('gender', 'female', true); !!}
-                {!!  Form::radio('gender', 'male', true);   !!}
-
         {!!  Form::label('phone', 'phone')!!}
             {!! Form::text('phone',$value=$user->phone,['class' => 'form-control']);!!}
 
@@ -58,53 +53,11 @@
             {!! Form::text('admission',$value=null,['id' => 'datepicker-3','class'=>'form-control'])!!}
 
         {!!  Form::label('main_image', 'Main_image') !!}
-            {!! Form::file('main_image'),['class'=>'form-control']!!}
-            {!! link_to("/patient/create/".$user->id, $title = "add reservation", $attributes = array(), $secure = null)!!}
+            {!! Form::file('main_image')!!}
 
-<<<<<<< HEAD
-
-{!!  Form::label('patientheight', 'patientheight') !!}
-{!! Form::text('patientheight',$value=$patientprofile->patientheight,['class' => 'form-control'])!!}
-
-
-{!!  Form::label('patientbloodgroup', 'patientbloodgroup') !!}
-{!! Form::text('patientbloodgroup',$value=$patientprofile->patientbloodgroup,['class' => 'form-control'])!!}
-
-{!! Form::select('size', array('A+' => 'A+', 'B+' => 'B+' , 'AB+' => 'AB+' , 'O+' => 'O+', 'O-' => 'O-' , 'A-' => 'A-'  ,'B-' => 'B-','AB-'=>'AB-'), $patientprofile->patientbloodgroup, ['placeholder' => 'Pick a size...']);!!}
-
-{!!  Form::label('patientemergencyphone', 'patientemergencyphone') !!}
-{!! Form::text('patientemergencyphone',$value=$patientprofile->patientemergencyphone,['class' => 'form-control'])!!}
-
-
-
-{!!  Form::label('patientnationality', 'patientnationality') !!}
-{!! Form::text('patientnationality',$value=$patientprofile->patientnationality,['class' => 'form-control'])!!}
-
-
-{!!  Form::label('patientnationalid', 'patientnationalid') !!}
-{!! Form::text('patientnationalid',$value=$patientprofile->patientnationalid,['class' => 'form-control'])!!}
-
-{!!  Form::label('admission_date', 'admission') !!}
-{!! Form::text('admission',$value=null,['id' => 'datepicker-3','class'=>'form-control'])!!}
-
-
-
-{!!  Form::label('main_image', 'Main_image') !!}
-{!! Form::file('main_image')!!}
-
-
-{!! link_to("/patient/create/".$user->id, $title = "add reservation", $attributes = array(), $secure = null)!!}
-
-
-{!! Form::submit('Update')!!}
-
-
-{!! Form::close() !!}
-=======
         {!! Form::submit('Update')!!}
     
     {!! Form::close() !!}
->>>>>>> f7057344ea659c547400b47020b2380e97732f1b
 
 @stop
 
